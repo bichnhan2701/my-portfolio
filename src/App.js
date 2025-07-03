3c3a44deefbe351a69f5ff6ff3182aa391cb69b1
bichnhan2701 <truongtuelam43@gmail.com>
2025-07-03 17:08:58 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import AboutMe  from "./components/AboutMe";
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Portfolio</h1>
       </header>
+
+      <main>
+        <AboutMe />
+
+        <section id="projects">
+          <h2>Projects</h2>
+          <p>Here are some projects I'v work on: </p>
+        </section>
+
+        <section id="contact">
+          <h2>contact me</h2>
+          <p>Email: </p>
+        </section>
+      </main>
+
+      <footer>
+        <p> Bich Nhan 2025 </p>
+      </footer>
+
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutMe = () => {
+    return (
+        <section id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm [Bich Nhan]. I enjoy building web applications and learning new technologies!</p>
+    </section>
+    );
+};
+
+export default AboutMe;
